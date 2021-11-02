844bbbb51d7229a0f57514b5e5d01b3bb31dc9cc
Leo Chen <chenl3@rose-hulman.edu>
2021-11-02 10:00:26 -0700

    commit tree 7591ee70823db3e633415a87c3fc2ffa615a265e

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,17 @@
 class Person
+    def name=(input_name)
+        @name = input_name
+    end
 
+    def name
+        @name
+    end
+
+    def job=(input_job)
+        @job = input_job
+    end
+
+    def job
+        @job
+    end
 end
